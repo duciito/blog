3433f8917200865ff1f324742e8596bffffb9c01
duciito <duciito@gmail.com>
2020-10-05 18:50:59 +0300

    Initial set of models and migration

--- core/models.py
+++ core/models.py
@@ -16,31 +16,40 @@ class BlogUser(User):
     )
 
 
+class EditableModel(models.Model):
+    creator = models.ForeignKey(
+        BlogUser,
+        on_delete=models.CASCADE,
+        related_name='%(class)s'
+    )
+    text = models.TextField()
+    posted_at = models.DateTimeField(auto_now_add=True, null=True)
+    # This one triggers differently.
+    # It updated the field on each save.
+    edited_at = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
 
-class Article(models.Model):
-    creator = models.ForeignKey(
-        BlogUser,
-        on_delete=models.CASCADE,
-        related_name='articles'
-    )
+class Article(EditableModel):
     category = models.ForeignKey(
         Category,
         related_name='articles',
         null=True,
         on_delete=models.SET_NULL
     )
-    voters = models.ManyToManyField(BlogUser, related_name='voted_articles', blank=True)
+    voters = models.ManyToManyField(BlogUser, related_name='liked_articles', blank=True)
     title = models.CharField(max_length=255)
-    posted_at = models.DateTimeField(auto_now_add=True, null=True)
     thumbnail_url = models.URLField(
         max_length=500,
         blank=True,
         default=f'{settings.SITE_URL}{settings.STATIC_URL}images/default-article.png'
     )
-    text = models.TextField()
 
 
 class ArticleContent(models.Model):
@@ -56,3 +65,12 @@ class ArticleContent(models.Model):
     url = models.URLField(max_length=500)
     guid = models.UUIDField(null=True, default=uuid.uuid4, unique=True)
     order_index = models.PositiveIntegerField()
+
+
+class Comment(EditableModel):
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    voters = models.ManyToManyField(BlogUser, related_name='liked_comments', blank=True)

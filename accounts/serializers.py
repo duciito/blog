b6e912c199890cc1736ac21084731cc042760f55
duciito <duciito@gmail.com>
2020-10-09 15:13:11 +0300

    Add sign up and logout functionality

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,9 +2,13 @@ from rest_framework.authtoken.models import Token
 from rest_framework import serializers, validators
 
 from accounts.models import BlogUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth import password_validation
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """Base user serializer."""
+
     class Meta:
         model = BlogUser
         fields = ('id',
@@ -20,6 +24,8 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class LoginSerializer(serializers.Serializer):
+    """Login credentials serializer."""
+
     email = serializers.EmailField(
         max_length=300,
         required=True,
@@ -33,10 +39,58 @@ class LoginSerializer(serializers.Serializer):
     password = serializers.CharField(required=True, write_only=True)
 
 
+class SignupSerializer(serializers.ModelSerializer):
+    """Signup user data serializer."""
+
+    class Meta:
+        model = BlogUser
+        fields = ('username', 'email',
+                'password', 'first_name',
+                'last_name', 'profile_description')
+
+    def validate_email(self, value):
+        """Check to see if taken."""
+
+        # `filter` instead of `get` to not get an exception.
+        user = BlogUser.objects.filter(email=value)
+        if user:
+            raise serializers.ValidationError("Email already taken!")
+        return BaseUserManager.normalize_email(value)
+
+    def validate_password(self, value):
+        """Validate password according to validators declared in settings."""
+
+        password_validation.validate_password(value)
+        return value
+
+    def create(self, validated_data):
+        return BlogUser.objects.create_user(**validated_data)
+
+
 class AuthUserSerializer(UserSerializer):
+    """Used to serialize data of successfully authenticated users."""
+
     auth_token = serializers.SerializerMethodField()
 
     def get_auth_token(self, user):
         token = Token.objects.get_or_create(user=user)
         return token.key
 
+
+class PasswordChangeSerializer(serializers.Serializer):
+    current_password = serializers.CharField(required=True, write_only=True)
+    new_password = serializers.CharField(required=True, write_only=True)
+
+    def validate_current_password(self, value):
+        # Request is passed with context by default
+        # with recent versions of Django.
+        if not self.context['request'].user.check_password(value):
+            raise serializers.ValidationError('Current password does not match!')
+
+        return value
+
+    def validate_new_password(self, value):
+        """Validate password similar to how it's validated when signing up."""
+
+        password_validation.validate_password(value)
+        return value
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,20 @@
 from django.core.exceptions import ImproperlyConfigured
-from django.contrib.auth import authenticate
-from rest_framework import viewsets, serializers
+from django.contrib.auth import authenticate, logout
+from rest_framework import serializers, status, viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from accounts.serializers import LoginSerializer, AuthUserSerializer
+from accounts.serializers import (
+    LoginSerializer, SignupSerializer, AuthUserSerializer
+)
 
 
 class AuthViewSet(viewsets.GenericViewSet):
     permission_classes = (AllowAny,)
     serializer_classes = {
-        'login': LoginSerializer
+        'login': LoginSerializer,
+        'signup': SignupSerializer
     }
 
     @action(methods=['POST'])
@@ -22,6 +25,20 @@ class AuthViewSet(viewsets.GenericViewSet):
         user = self._get_and_auth_user(**serializer.validated_data)
         success_data = AuthUserSerializer(user).data
         return Response(data=success_data)
+
+    @action(methods=['POST'])
+    def signup(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.save()
+        success_data = AuthUserSerializer(user).data
+        return Response(data=success_data, status=status.HTTP_201_CREATED)
+
+    @action(methods=['POST'])
+    def logout(self, request):
+        logout(request)
+        return Response(data={'success': 'Logged out successfully!'})
 
     def get_serializer_class(self):
         if not isinstance(self.serializer_classes, dict):

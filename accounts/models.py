e149d71fb2773962695d7cb24bc58559e88ff0fa
duciito <duciito@gmail.com>
2020-10-15 21:20:53 +0300

    Fix migrations to allow auth user to be custom

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
-class BlogUser(User):
+class BlogUser(AbstractUser):
+    email = models.EmailField(unique=True)
     profile_description = models.TextField(blank=True)
     joined_at = models.DateTimeField(auto_now_add=True, null=True)
     followed_users = models.ManyToManyField(
@@ -14,3 +15,6 @@ class BlogUser(User):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ()

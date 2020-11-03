d97ea44ccc3f90cdf8de92965ddbce94d6153bfa
duciito <duciito@gmail.com>
2020-11-04 00:09:08 +0200

    Get hot articles based on votes and comments

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+
+from django.db.models import Count
+from django.utils import timezone
 from rest_framework import response, viewsets, status
 from rest_framework.decorators import action
 
@@ -26,19 +30,40 @@ class ArticlesViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['post'])
     def vote(self, request, pk=None):
+        """Vote for an article."""
         article = self.get_object()
         article.voters.add(request.user)
         return response.Response(status=status.HTTP_204_NO_CONTENT)
 
     @action(detail=True, methods=['post'])
     def unvote(self, request, pk=None):
+        """
+        Remove vote for an article.
+        Doesn't thrown an exception if the user hasn't voted.
+        """
         article = self.get_object()
         article.voters.remove(request.user)
         return response.Response(status=status.HTTP_204_NO_CONTENT)
 
-    @action(detail=False, methods=['post'])
+    @action(detail=False, methods=['get'])
     def hot(self, request):
-        pass
+        """Get articles that have gained popularity quickly."""
+        last_three_days = timezone.now() - timedelta(days=3)
+        serializer = self.get_serializer_class()
+        queryset = self.get_queryset()
+
+        queryset = queryset.annotate(
+            num_voters=Count('voters'),
+            num_comments=Count('comments')
+        ).filter(
+            num_voters__gte=5,
+            num_comments__gt=2,
+            posted_at__gte=last_three_days
+        )
+
+        # Serialize new queryset using view's serializer.
+        result = serializer(queryset, many=True)
+        return response.Response(result.data)
 
 
 class ArticleContentsViewSet(viewsets.ModelViewSet):

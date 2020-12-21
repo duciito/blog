67dcc2bd31da998080d45c1decc7f1a795764557
duciito <duciito@gmail.com>
2020-12-21 23:53:47 +0200

    Add component logic for login

--- blog-client/src/app/login/login.component.ts
+++ blog-client/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AccountService} from '../services/account.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  returnUrl: string;
+
+  constructor(
+    private accountService: AccountService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    // Get return url or redirect to home if none
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  get f() {return this.form.controls;}
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.accountService.login(this.f.username.value, this.f.password.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        }
+      )
   }
 
 }

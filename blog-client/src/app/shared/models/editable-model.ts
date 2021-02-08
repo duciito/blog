5243947f0c2e3178c4801a277dbdf9527ccd3c5e
duciito <duciito@gmail.com>
2021-02-08 22:54:23 +0200

    Allow editable model interface to accept User as a type of the creator

--- blog-client/src/app/shared/models/editable-model.ts
+++ blog-client/src/app/shared/models/editable-model.ts
@@ -1,6 +1,8 @@
+import {User} from 'src/app/core/models/user';
+
 export interface EditableModel {
   id?: number;
-  creator: number;
+  creator: number | User;
   text?: string;
   posted_at?: Date;
   edited_at?: Date;
